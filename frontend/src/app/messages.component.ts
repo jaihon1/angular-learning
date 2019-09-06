45ea5b4ca19d9f8c6fc07dc01c0aa1efc603f8bc
Jaihon <jaihon.qasim@hotmail.com>
2019-09-06 00:10:23 -0400

    First take at frontend.

---
+++ frontend/src/app/messages.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'messages',
+    template: `
+    <div *ngFor="let message of messages">
+        <mat-card style="margin:8px;">
+            <mat-card-title>{{message.owner}}</mat-card-title>
+            <mat-card-content>
+                <p>
+                    {{message.text}}
+                </p>
+            </mat-card-content>
+        </mat-card>
+    </div>`
+
+})
+
+export class MessagesComponent {
+    messages = [{text: 'some text', owner: 'Tim'}, {text: 'some other text', owner: 'Jim'}];
+
+}
+
